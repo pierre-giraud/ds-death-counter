23656ce24696b3403c0c61ca3c7c6fe5a0e7a86b
Pierre Giraud <giraudp1997@gmail.com>
2020-11-27 23:36:45 +0100

    Test resolution deploiement

--- pages/api/boss/index.js
+++ pages/api/boss/index.js
@@ -2,9 +2,10 @@ import nextConnect from 'next-connect';
 
 const dbModel = require("../../../db/db");
 
+/*
 const handler = nextConnect()
     .get(async (req, res) => {
-        // Récupération de tous les joueurs
+        // Récupération de tous les boss
         const bossesObj = await dbModel.db['bosses'].findAll();
         const bosses = JSON.stringify(bossesObj);
 
@@ -23,4 +24,30 @@ const handler = nextConnect()
         res.statusCode = 200;
     });
 
-export default handler;
+export default handler;*/
+
+export default function handler(req, res) {
+    if (req.method === 'GET'){
+        // Récupération de tous les boss
+        dbModel.db['bosses'].findAll().then((b) => {
+            const bosses = JSON.stringify(b);
+
+            res.status(200).json(bosses);
+        }).catch((err) => {
+            res.status(err.statusCode).json({});
+        });
+    } else if (req.method === 'POST'){
+        const { body } = req;
+        const { name } = body;
+
+        dbModel.db["bosses"].create({
+            name: name,
+        }).then((b) => {
+            const boss = JSON.stringify(b);
+
+            res.status(200).json(boss);
+        }).catch((err) => {
+            res.status(err.statusCode).json({});
+        });
+    }
+}
--- pages/api/player/index.js
+++ pages/api/player/index.js
@@ -2,7 +2,7 @@ import nextConnect from 'next-connect';
 
 const dbModel = require("../../../db/db");
 
-const handler = nextConnect()
+/*const handler = nextConnect()
     .get(async (req, res) => {
         // Récupération de tous les joueurs
         const playersObj = await dbModel.db['players'].findAll();
@@ -23,4 +23,28 @@ const handler = nextConnect()
         res.json(newPlayer);
     });
 
-export default handler;
+export default handler;*/
+
+/*export default function handler(req, res) {
+    if (req.method === 'GET'){
+        // Récupération de tous les joueurs
+        const playersObj = dbModel.db['players'].findAll();
+        const players = JSON.stringify(playersObj);
+
+        res.statusCode = 200;
+        res.json(players);
+    }
+}*/
+
+export default function handler(req, res) {
+    if (req.method === 'GET'){
+        // Récupération de tous les joueurs
+        dbModel.db['players'].findAll().then((p) => {
+            const players = JSON.stringify(p);
+
+            res.status(200).json(players);
+        }).catch((err) => {
+            res.status(err.statusCode).json({});
+        });
+    }
+}

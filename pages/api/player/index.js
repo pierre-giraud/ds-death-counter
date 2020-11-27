505ce821a9b314e26a90a327ad49e37d9c2ae5c7
Pierre Giraud <giraudp1997@gmail.com>
2020-11-27 23:02:30 +0100

    Test resolution deploiement

--- pages/api/player/index.js
+++ pages/api/player/index.js
@@ -5,8 +5,10 @@ const dbModel = require("../../../db/db");
 const handler = nextConnect()
     .get(async (req, res) => {
         // Récupération de tous les joueurs
+        console.log("APPEL PLAYERS");
         const playersObj = await dbModel.db['players'].findAll();
         const players = JSON.stringify(playersObj);
+        console.log("FIN APPEL PLAYERS");
 
         res.statusCode = 200;
         res.json(players);
